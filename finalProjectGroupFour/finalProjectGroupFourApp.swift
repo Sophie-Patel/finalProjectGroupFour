e7868e657f3f1ca6417412782cf7ce89201aa659
Sophie Patel <sophie.e.k.patel@gmail.com>
2023-08-01 09:35:49 +0100

    Initial Commit

---
+++ finalProjectGroupFour/finalProjectGroupFourApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectGroupFourApp.swift
+//  finalProjectGroupFour
+//
+//  Created by scholar on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectGroupFourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
